8419f44737e3bdbebb390fc372718568bd6993b4
LuisGalindez <125323011+LuisGalindez@users.noreply.github.com>
2023-02-18 15:48:51 -0400

    Add files via upload

---
+++ Figura/Prepa 03/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figuras_Luis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangulo rectangulo_cuadrado = new Cuadrado(5, "azul");
+            Rectangulo rectangulo = new Rectangulo(2,3, "blanco");
+
+            Figura figura1 = rectangulo_cuadrado;
+            Figura figura2 = rectangulo;
+
+            Figura figura3 = new Circulo(2, "negro");
+            Figura figura4 = new Triangulo(7, 4.03, 3, 6, "rojo");
+            Figura figura5 = new Isosceles(10, 8, 5, "verde");
+            Figura figura6 = new Escaleno(13, 7.371, 12.01, 10.89, "amarillo");
+            Figura figura7 = new Equilatero(8.66, 10, "naranja");
+
+
+            figura1.MostrarDatos();
+          
+            figura2.MostrarDatos();
+           
+            figura3.MostrarDatos();
+         
+            figura4.MostrarDatos();
+
+            figura5.MostrarDatos();
+            
+            figura6.MostrarDatos();
+            
+            figura7.MostrarDatos();
+
+
+
+            Console.ReadLine();
+           
+            
+        }
+    }
+}
---
+++ Figura/Prepa 03/Triangulo.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prepa_03
+{
+    class Triangulo : Figura
+    {
+        private double _base;
+        private double _altura;
+        private double _lado1;
+        private double _lado2;
+        public Triangulo(double base_triangulo, double altura, double lado1, double lado2, string color) : base(color)
+        {
+            _base = base_triangulo;
+            _altura = altura;
+            _lado1 = lado1;
+            _lado2 = lado2;
+        }
+
+        public override double GetArea()
+        {
+            return (_base * _altura) / 2;
+        }
+        public override double GetPerimetro()
+        {
+            return _lado1 + _base + _lado2;
+        }
+        public override void MostrarDatos()
+        {
+            Console.WriteLine($" Soy un Triangulo de Color {GetColor()}, Area {GetArea()}, Perimetro {GetPerimetro()}");
+        }
+    }
+
+    class Isosceles : Triangulo
+    {
+        public Isosceles(double base_triangulo, double altura, float lado, string color) : base(base_triangulo, altura, lado, lado, color)
+        {
+
+        }
+        public override void MostrarDatos()
+        {
+            Console.WriteLine($" Soy un Triangulo Isosceles de Color {GetColor()}, Area {GetArea()}, Perimetro {GetPerimetro()}");
+        }
+    }
+    class Escaleno : Triangulo
+    {
+        public Escaleno(double base_triangulo, double altura, double lado1, double lado2, string color) : base(base_triangulo, altura, lado1, lado2, color)
+        {
+
+        }
+        public override void MostrarDatos()
+        {
+            Console.WriteLine($" Soy un Triangulo escaleno de Color {GetColor()}, Area {GetArea()}, Perimetro {GetPerimetro()}");
+        }
+
+    }
+    class Equilatero : Triangulo
+    {
+        public Equilatero(double altura, double lado, string color) : base(lado, altura, lado, lado, color)
+        {
+
+        }
+        public override void MostrarDatos()
+        {
+            Console.WriteLine($" Soy un Triangulo equilatero de Color {GetColor()}, Area {GetArea()}, Perimetro {GetPerimetro()}");
+        }
+
+    }
+}
